53b54f76879b06447db91328b2ead1ec11833057
Eli Jackson <tucker.eli.jackson@gmail.com>
2018-03-07 15:39:39 -0600

    more tests added

--- test/parse-args-test.js
+++ test/parse-args-test.js
@@ -1,6 +1,6 @@
 
 const parseArgs = require('../lib/parse-args.js')
-const { assert: { isFunction, isNotNaN, isString, isObject } } = require('chai');
+const { assert: { isFunction, isNotNaN, isString, isObject, deepEqual } } = require('chai');
 
 describe('parse-args module', () => {
     let args = [1, 6];
@@ -8,8 +8,12 @@ describe('parse-args module', () => {
         it('should be a function lol', () => {
             isFunction(parseArgs);
         });
-        it('returns an object', () => {
+        it('should return an object', () => {
             isObject(parseArgs(args));
         })
-    })
+        it('should return an object of 1 and 6 if no arguments are provided', () => {
+            args = [];
+            deepEqual(parseArgs(args), {"count": 1, "sides": 6});
+            })
+        })
 })
